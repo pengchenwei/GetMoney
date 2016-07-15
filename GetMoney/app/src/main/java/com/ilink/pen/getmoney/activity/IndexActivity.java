c5f96b82295a11ce4400f2a474fb852d344542bf
pengchenwei <1061029525@qq.com>
2016-07-15 16:58:07 +0800

    退出方式：1对话框确定退出2.连击两次时间小于2秒

--- GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/IndexActivity.java
+++ GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/IndexActivity.java
@@ -1,8 +1,11 @@
 package com.ilink.pen.getmoney.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,8 +16,8 @@ import com.ilink.pen.getmoney.R;
  * Created by pen on 2016/7/14.
  */
 public class IndexActivity extends AppCompatActivity {
-    private Button btnToMain, btnToSecond,btnToSome,btnToActivityLifeCycle,
-            btnToCapture,btnToBrodcastSender,btnToDynamicRegisterBrodcast;
+    private Button btnToMain, btnToSecond, btnToSome, btnToActivityLifeCycle,
+            btnToCapture, btnToBrodcastSender, btnToDynamicRegisterBrodcast;
     private BtnListener btnListener;
 
     @Override
@@ -27,6 +30,7 @@ public class IndexActivity extends AppCompatActivity {
         FindView();
         SetClickListener();
     }
+
     // 通过id获得View
     private void FindView() {
         btnToMain = (Button) findViewById(R.id.btnToMain);
@@ -40,6 +44,7 @@ public class IndexActivity extends AppCompatActivity {
         btnToBrodcastSender = (Button) findViewById(R.id.btnToBrodcastSender);
         btnToDynamicRegisterBrodcast = (Button) findViewById(R.id.btnToDynamicRegisterBrodcast);
     }
+
     // 绑定监听器
     private void SetClickListener() {
         btnToMain.setOnClickListener(btnListener);
@@ -53,7 +58,8 @@ public class IndexActivity extends AppCompatActivity {
         btnToDynamicRegisterBrodcast.setOnClickListener(btnListener);
 
     }
-// 内部类来简化btnListener，将FindView中得到的view传入，用switch/case来判断onClick事件
+
+    // 内部类来简化btnListener，将FindView中得到的view传入，用switch/case来判断onClick事件
     private class BtnListener implements View.OnClickListener {
 
         @Override
@@ -90,5 +96,43 @@ public class IndexActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    private long exitTime = 0;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //退出方式一：确认退出对话框
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            AlertDialog.Builder exitDialog = new AlertDialog.Builder(this);
+            exitDialog.setTitle("提示");
+            exitDialog.setMessage("确定退出么？");
+            exitDialog.setPositiveButton("退出", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            exitDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    return;
+                }
+            });
+            exitDialog.show();
+        }
+        //退出方式二：连按两次返回
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 2000) {//判断连击两次的时间间隔大于2秒就弹出提示
+                Toast.makeText(IndexActivity.this, "再按一次退出应用", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {//否则表示连击两次小于2秒，退出
+                finish();
+                System.exit(0);
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
