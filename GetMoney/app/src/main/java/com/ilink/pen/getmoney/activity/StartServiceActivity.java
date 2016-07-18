94ca4cc43226d9ffcedd7616d0c4a923e44d0ac1
pengchenwei <1061029525@qq.com>
2016-07-18 17:21:16 +0800

    bindService
    绑定服务

--- GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/StartServiceActivity.java
+++ GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/StartServiceActivity.java
@@ -1,20 +1,40 @@
 package com.ilink.pen.getmoney.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.ilink.pen.getmoney.R;
+import com.ilink.pen.getmoney.service.BindService;
 import com.ilink.pen.getmoney.service.StartService;
 
 public class StartServiceActivity extends AppCompatActivity {
+    private BindService.MyBinder mybander;
+    boolean isBind = false;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        //service连接后会调用onServiceConnected
+        @Override
+        //通过IBinder与service进行通信
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mybander = (BindService.MyBinder) service;
+            mybander.startDownload();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_service);
-
+//启动服务
         this.findViewById(R.id.btnStartService).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,12 +42,31 @@ public class StartServiceActivity extends AppCompatActivity {
                 startService(startSerIntent);
             }
         });
-
+//停止服务
         this.findViewById(R.id.btnStopService).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent stopIntent= new Intent(StartServiceActivity.this, StartService.class);
+                Intent stopIntent = new Intent(StartServiceActivity.this, StartService.class);
                 stopService(stopIntent);
+            }
+        });
+//绑定服务
+        this.findViewById(R.id.btnBindService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bindIntent = new Intent(StartServiceActivity.this, BindService.class);
+                //绑定上面创建的serviceConnection，BIND_AUTO_CREATE（创建后自动执行）
+                isBind = bindService(bindIntent, serviceConnection, BIND_AUTO_CREATE);//绑定成功返回一个true
+            }
+        });
+//解绑服务
+        this.findViewById(R.id.btnUnbindService).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isBind) {//没有绑定就不能执行解绑操作
+                    unbindService(serviceConnection);
+                    isBind = false;
+                }
             }
         });
     }
---
+++ GetMoney/app/src/main/java/com/ilink/pen/getmoney/service/BindService.java
@@ -0,0 +1,52 @@
+package com.ilink.pen.getmoney.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by pen on 2016/7/18.
+ */
+public class BindService extends Service {
+    private String TAG = "BindService";
+
+    MyBinder myBinder = new MyBinder();
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "BindService-->onCreate");//调试窗口打印日志观察
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "BindService-->onStartCommand");//只有startService才会调用
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "BindService-->onDestroy");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "BindService-->onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "BindService-->onBind");
+        return myBinder;
+    }
+
+    public class MyBinder extends Binder {
+        public void startDownload() {
+            Log.i(TAG, "开始进行下载操作。。。");
+        }
+    }
+}
