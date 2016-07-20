f4cf710954a2eb67d89a12dbdfb241610b5133be
pengchenwei <p1587600993@Gmail.com>
2016-07-20 22:24:36 +0800

    fragment
    完成fragment控件的学习

--- GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/FragmentActivity.java
+++ GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/FragmentActivity.java
@@ -4,27 +4,71 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.ilink.pen.getmoney.R;
 
 public class FragmentActivity extends AppCompatActivity {
+    public FragmentActivity() {
+        super();
+    }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("FragmentActivity", "onStop被执行了");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("FragmentActivity", "onDestroy被执行了");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("FragmentActivity", "onPause被执行了");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("FragmentActivity", "onResume被执行了");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("FragmentActivity", "onStart被执行了");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("FragmentActivity", "onRestart被执行了");
+    }
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("FragmentActivity", "onCreate被执行了");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
         Button btnChangeRightFragment = (Button) findViewById(R.id.btnChangeRightFragment);
-        btnChangeRightFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AnotherRightFragment anotherRightFragment = new AnotherRightFragment();
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.flRight, anotherRightFragment);
-            }
-        });
+        if (btnChangeRightFragment != null) {
+            btnChangeRightFragment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AnotherRightFragment anotherRightFragment = new AnotherRightFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.flRight, anotherRightFragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            });
+        }
 
     }
 }
--- GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/LeftFragment.java
+++ GetMoney/app/src/main/java/com/ilink/pen/getmoney/activity/LeftFragment.java
@@ -3,7 +3,9 @@ package com.ilink.pen.getmoney.activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +21,79 @@ public class LeftFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.e("LeftFragment", "onAttach()");
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.e("LeftFragment", "onCreate()");
+        super.onCreate(savedInstanceState);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Log.e("LeftFragment", "onCreateView()");
         return inflater.inflate(R.layout.fragment_left, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.e("LeftFragment", "onActivityCreated()");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.e("LeftFragment", "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.e("LeftFragment", "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.e("LeftFragment", "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e("LeftFragment", "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.e("LeftFragment", "onDestroyView()");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.e("LeftFragment", "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.e("LeftFragment", "onDetach()");
+        super.onDetach();
     }
 
 }
